5645592b21a94daf9dd228f56042b6262e5ff1ba
AdejuwonAdepate <102380094+AdejuwonAdepate@users.noreply.github.com>
2025-09-10 09:19:33 +0100

    edited the privacy-policy page

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import Footer from "../../Components/Footer/Footer";
 
 export default function TermsOfUse() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <div className="container mx-auto px-4 py-8 max-w-4xl">
-        <div className="shadow-lg">
+      <Navbar />
+      <div className="container mx-auto px-4 py-8 max-w-7xl">
+        <div className="">
           <div className="p-6 md:p-8 lg:p-12">
             <div className="prose prose-gray max-w-none">
               <div className="text-center mb-8">
@@ -1619,6 +1622,7 @@ export default function TermsOfUse() {
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
