a0d5a9643a588fe776a1e30f5842510d58f12357
AdejuwonAdepate <102380094+AdejuwonAdepate@users.noreply.github.com>
2025-10-06 05:48:39 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-"use client"
-import React from "react"
-import Navbar from "../Components/Navbar/Navbar"
-import Banner from "./components/banner"
-import Footer from "../Components/Footer/Footer"
-import Refer from "../Components/Refer/Refer"
-import Testimonial from "./components/testimonial"
-import Services from "./components/services"
-import ChooseUs from "./components/chooseUs"
-import Reward from "./components/reward"
-import Category from "./components/category"
+"use client";
+import React from "react";
+import Navbar from "../Components/Navbar/Navbar";
+import Banner from "./components/banner";
+import Footer from "../Components/Footer/Footer";
+import Refer from "../Components/Refer/Refer";
+import Testimonial from "./components/testimonial";
+import Services from "./components/services";
+import ChooseUs from "./components/chooseUs";
+import Reward from "./components/reward";
+import Category from "./components/category";
 import Products from "../app/components/products";
-import "./home.css"
+import "./home.css";
 
 export default function Home() {
   return (
@@ -18,7 +18,7 @@ export default function Home() {
       <Navbar />
       <Banner />
       <Services />
-      <Category />
+      {/* <Category /> */}
       <Products />
       <ChooseUs />
       <Reward />
