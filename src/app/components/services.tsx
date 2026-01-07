62a916c3be82384a1ba28d7f2f78c365e37f8ec5
Adejuwon Adepate <adepateadejuwon@gmail.com>
2026-01-07 22:17:01 +0100

    services component ui update.

--- src/app/components/services.tsx
+++ src/app/components/services.tsx
@@ -9,8 +9,8 @@ import profimg4 from "../../Assets/customer.png";
 
 const services = () => {
   return (
-    <div className="px-4 py-10 md:px-9 md:py-10">
-      <div className="max-w-6xl mx-auto">
+    <div className="px-4 py-6 md:px-9 md:py-6 bg-[#ffffff]">
+      <div className="w-wull mx-auto">
         <h3 className="text-2xl md:text-3xl font-semibold mb-8">Services</h3>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-5">
@@ -22,14 +22,14 @@ const services = () => {
               className="w-full h-auto max-h-40 md:max-h-none object-cover rounded-2xl"
             />
             <div className="mt-3 md:mt-4 text-center md:text-left w-full">
-              <h4 className="text-base md:text-lg font-semibold text-gray-900">
+              <h4 className="text-base md:text-lg font-semibold text-[#281D1B]">
                 Products
               </h4>
-              <p className="text-[#281d1b] text-xs md:text-sm mt-2 md:mt-2.5 leading-relaxed">
+              <p className="text-[#281d1b] text-xs md:text-lg mt-2 md:mt-2.5 leading-relaxed">
                 Revolutionise your energy use with our cutting-edge solar
                 solutions.
               </p>
-              <button className="w-full md:w-auto bg-red-600 hover:bg-red-700 text-white font-bold text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors">
+              <button className="w-full md:w-auto bg-[#FF0000] hover:bg-red-700 text-white text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors">
                 Shop Now
               </button>
             </div>
@@ -43,15 +43,15 @@ const services = () => {
               className="w-full h-auto max-h-40 md:max-h-none object-cover rounded-2xl"
             />
             <div className="mt-3 md:mt-4 text-center md:text-left w-full">
-              <h4 className="text-base md:text-lg font-semibold text-gray-900">
+              <h4 className="text-base md:text-lg font-semibold text-[#281D1B]">
                 Installation
               </h4>
-              <p className="text-[#281d1b] text-xs md:text-sm mt-2 md:mt-2.5 leading-relaxed">
+              <p className="text-[#281d1b] text-xs md:text-lg mt-2 md:mt-2.5 leading-relaxed">
                 Enjoy exceptional solar installations that illuminate your
                 space.
               </p>
-              <button className="w-full md:w-auto bg-red-600 hover:bg-red-700 text-white font-bold text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors">
-                Learn More
+              <button className="w-full md:w-auto bg-[#FF0000] hover:bg-red-700 text-white text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors">
+               Shop Now
               </button>
             </div>
           </div>
@@ -64,14 +64,14 @@ const services = () => {
               className="w-full h-auto max-h-40 md:max-h-none object-cover rounded-2xl"
             />
             <div className="mt-3 md:mt-4 text-center md:text-left w-full">
-              <h4 className="text-base md:text-lg font-semibold text-gray-900">
-                Usage
+              <h4 className="text-base md:text-lg font-semibold text-[#281D1B]">
+                Referral & Rewards
               </h4>
-              <p className="text-[#281d1b] text-xs md:text-sm mt-2 md:mt-2.5 leading-relaxed">
-                Track your solar energy usage effortlessly and stay informed.
+              <p className="text-[#281d1b] text-xs md:text-lg mt-2 md:mt-2.5 leading-relaxed">
+                Share your referral code, and earn rewards.
               </p>
-              <button className="w-full md:w-auto bg-red-600 hover:bg-red-700 text-white font-bold text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors">
-                Learn More
+              <button className="w-full md:w-auto bg-[#FF0000] hover:bg-red-700 text-white  text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors">
+               Refer a Friend
               </button>
             </div>
           </div>
@@ -84,14 +84,13 @@ const services = () => {
               className="w-full h-auto max-h-40 md:max-h-none object-cover rounded-2xl"
             />
             <div className="mt-3 md:mt-4 text-center md:text-left w-full">
-              <h4 className="text-base md:text-lg font-semibold text-gray-900">
+              <h4 className="text-base md:text-lg font-semibold text-[#281D1B]">
                 Enquiries
               </h4>
-              <p className="text-[#281d1b] text-xs md:text-sm mt-2 md:mt-2.5 leading-relaxed">
-                Inquire about our solar solutions and get expert assistance
-                today!
+              <p className="text-[#281d1b] text-xs md:text-lg mt-2 md:mt-2.5 leading-relaxed">
+                Inquire about our solar solutions and get expert assistance today!
               </p>
-              <button className="w-full md:w-auto bg-red-600 hover:bg-red-700 text-white font-bold text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors">
+              <button className="w-full md:w-auto bg-[#FF0000] hover:bg-red-700 text-white text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors">
                 Contact Us
               </button>
             </div>
