5dc10a6611452aeaac9798bc9c05a9b4f8972201
Adejuwon Adepate <adepateadejuwon@gmail.com>
2026-01-08 09:40:21 +0100

    adjusted services button

--- src/app/components/services.tsx
+++ src/app/components/services.tsx
@@ -15,7 +15,7 @@ const services = () => {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-5">
           {/* Product Service */}
-          <div className="bg-[#fafafa] rounded-2xl p-3 flex flex-col items-center">
+          <div className="bg-[#fafafa] rounded-2xl p-3 pb-6 flex flex-col items-center">
             <Image
               src={profimg1}
               alt="Products"
@@ -25,18 +25,18 @@ const services = () => {
               <h4 className="text-base md:text-lg font-semibold text-[#281D1B]">
                 Products
               </h4>
-              <p className="text-[#281d1b] text-md md:text-md mt-2 md:mt-2.5 leading-relaxed">
+              <p className="text-[#281d1b] text-md md:text-md mt-2 md:mt-2.5 leading-relaxed pb-6">
                 Revolutionise your energy use with our cutting-edge solar
                 solutions.
               </p>
-              <button className="w-full md:w-auto bg-[#FF0000] hover:bg-red-700 text-white text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors">
+              <Link href="/products" className="w-full md:w-auto bg-[#FF0000] hover:bg-red-700 text-white text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors my-8 md:my-8">
                 Shop Now
-              </button>
+              </Link>
             </div>
           </div>
 
           {/* Installation Service */}
-          <div className="bg-[#fafafa] rounded-2xl p-3 flex flex-col items-center">
+          <div className="bg-[#fafafa] rounded-2xl p-3 pb-6 flex flex-col items-center">
             <Image
               src={profimg2}
               alt="Installation"
@@ -46,13 +46,13 @@ const services = () => {
               <h4 className="text-base md:text-lg font-semibold text-[#281D1B]">
                 Installation
               </h4>
-              <p className="text-[#281d1b] text-md md:text-md mt-2 md:mt-2.5 leading-relaxed">
+              <p className="text-[#281d1b] text-md md:text-md mt-2 md:mt-2.5 leading-relaxed pb-6">
                 Enjoy exceptional solar installations that illuminate your
                 space.
               </p>
-              <button className="w-full md:w-auto bg-[#FF0000] hover:bg-red-700 text-white text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors">
+              <Link href="/installation" className="w-full md:w-auto bg-[#FF0000] hover:bg-red-700 text-white text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors">
                Shop Now
-              </button>
+              </Link>
             </div>
           </div>
 
@@ -67,17 +67,17 @@ const services = () => {
               <h4 className="text-base md:text-lg font-semibold text-[#281D1B]">
                 Referral & Rewards
               </h4>
-              <p className="text-[#281d1b] text-md md:text-md mt-2 md:mt-2.5 leading-relaxed">
+              <p className="text-[#281d1b] text-md md:text-md mt-2 md:mt-2.5 leading-relaxed pb-6">
                 Share your referral code, and earn rewards.
               </p>
-              <button className="w-full md:w-auto bg-[#FF0000] hover:bg-red-700 text-white  text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors">
+              <Link href="/referral" className="w-full md:w-auto bg-[#FF0000] hover:bg-red-700 text-white  text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors">
                Refer a Friend
-              </button>
+              </Link>
             </div>
           </div>
 
           {/* Enquiries Service */}
-          <div className="bg-[#fafafa] rounded-2xl p-3 flex flex-col items-center">
+          <div className="bg-[#fafafa] rounded-2xl p-3 pb-6 flex flex-col items-center">
             <Image
               src={profimg4}
               alt="Enquiries"
@@ -87,12 +87,12 @@ const services = () => {
               <h4 className="text-base md:text-lg font-semibold text-[#281D1B]">
                 Enquiries
               </h4>
-              <p className="text-[#281d1b] text-md md:text-md mt-2 md:mt-2.5 leading-relaxed">
+              <p className="text-[#281d1b] text-md md:text-md mt-2 md:mt-2.5 leading-relaxed pb-6">
                 Inquire about our solar solutions and get expert assistance today!
               </p>
-              <button className="w-full md:w-auto bg-[#FF0000] hover:bg-red-700 text-white text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors">
+              <Link href="/contact" className="w-full md:w-auto bg-[#FF0000] hover:bg-red-700 text-white text-sm md:text-base px-6 md:px-8 py-2.5 md:py-2.5 rounded-full mt-3.5 md:mt-4 transition-colors">
                 Contact Us
-              </button>
+              </Link>
             </div>
           </div>
         </div>
